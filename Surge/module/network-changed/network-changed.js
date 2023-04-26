6dd9b4680b8fd04943ac72c9b346b44434d254a6
SimonGino <68073491+SimonGino@users.noreply.github.com>
2023-04-26 19:43:58 +0800

    Update network-changed.js

--- Surge/module/network-changed/network-changed.js
+++ Surge/module/network-changed/network-changed.js
@@ -1,36 +1,141 @@
 let config = {
     all_switch: ["Redmi_9944_5G", "Redmi_9944"],
+    all_proxy: '🔰 节点选择',
+    all_snell: '日本snell',
+    all_ss: '鸡总日本',
 };
+let $ = nobyda();
 
 (async () => {
-    const network = $network.wifi.ssid;
-    if (network) {
-        const isIncluded = config.all_switch.includes(network);
-        if (isIncluded) { //if the script was last run in cellular env.
-            await new Promise((resolve) => {
-                $httpAPI("POST", "v1/policy_groups/select", {
-                    group_name: '🔰 节点选择',
-                    policy: '日本snell'
-                }, (b) => resolve(!b.error || 0))
-            });
-            $notification.post('墙中墙坐牢模式开启,节点开始切换', '', `网络切换至 日本snell`);
+    const current = await $.getPolicy(config.all_proxy);
+    if(current === '鸡总日本'){
+        const network = $network.wifi.ssid;
+        if (network) {
+            const isIncluded = config.all_switch.includes(network);
+            if (isIncluded) { 
+                //if the script was last run in cellular env.
+                $.setPolicy(config.all_proxy,config.all_snell)
+                $notification.post('墙中墙模式🚫', '', `网络切换至 ${config.all_snell}`);
+            } else {
+                $.setPolicy(config.all_proxy,config.all_ss)
+                $notification.post('墙中墙模式✅', '', `网络切换至  ${config.all_ss}`);
+            }
         } else {
-            await new Promise((resolve) => {
-                $httpAPI("POST", "v1/policy_groups/select", {
-                    group_name: '🔰 节点选择',
-                    policy: '鸡总日本'
-                }, (b) => resolve(!b.error || 0))
-            });
-            $notification.post('墙中墙坐牢模式解除,节点开始切换', '', `网络切换至 鸡总日本`);
+            $.setPolicy(config.all_proxy,config.all_ss)
+            $notification.post('墙中墙模式✅', '', `网络切换至  ${config.all_ss}`);
         }
-    } else {
-        await new Promise((resolve) => {
-            $httpAPI("POST", "v1/policy_groups/select", {
-                group_name: '🔰 节点选择',
-                policy: '鸡总日本'
-            }, (b) => resolve(!b.error || 0))
-        });
-        $notification.post('墙中墙坐牢模式解除,节点开始切换', '', `网络切换至 鸡总日本`);
     }
 })().catch((err) => $notification.post('防火墙', '', `出现错误: ${err.message || err}`))
     .finally(() => $done({}))
+
+function nobyda() {
+    const isHTTP = typeof $httpClient != "undefined";
+    const isLoon = typeof $loon != "undefined";
+    const isQuanX = typeof $task != "undefined";
+    const isSurge = typeof $network != "undefined" && typeof $script != "undefined";
+    const ssid = (() => {
+        if (isQuanX && typeof ($environment) !== 'undefined') {
+            return $environment.ssid;
+        }
+        if (isSurge && $network.wifi) {
+            return $network.wifi.ssid;
+        }
+        if (isLoon) {
+            return JSON.parse($config.getConfig()).ssid;
+        }
+    })();
+    const notify = (title, subtitle, message) => {
+        console.log(`${title}\n${subtitle}\n${message}`);
+        if (isQuanX) $notify(title, subtitle, message);
+        if (isHTTP) $notification.post(title, subtitle, message);
+    }
+    const read = (key) => {
+        if (isQuanX) return $prefs.valueForKey(key);
+        if (isHTTP) return $persistentStore.read(key);
+    }
+    const adapterStatus = (response) => {
+        if (!response) return null;
+        if (response.status) {
+            response["statusCode"] = response.status;
+        } else if (response.statusCode) {
+            response["status"] = response.statusCode;
+        }
+        return response;
+    }
+    const getPolicy = (groupName) => {
+        if (isSurge) {
+            if (typeof ($httpAPI) === 'undefined') return 3;
+            return new Promise((resolve) => {
+                $httpAPI("GET", "v1/policy_groups/select", {
+                    group_name: encodeURIComponent(groupName)
+                }, (b) => resolve(b.policy || 2))
+            })
+        }
+        if (isLoon) {
+            if (typeof ($config.getPolicy) === 'undefined') return 3;
+            const getName = $config.getPolicy(groupName);
+            return getName || 2;
+        }
+        if (isQuanX) {
+            if (typeof ($configuration) === 'undefined') return 3;
+            return new Promise((resolve) => {
+                $configuration.sendMessage({
+                    action: "get_policy_state"
+                }).then(b => {
+                    if (b.ret && b.ret[groupName]) {
+                        resolve(b.ret[groupName][1]);
+                    } else resolve(2);
+                }, () => resolve());
+            })
+        }
+    }
+    const setPolicy = (group, policy) => {
+        if (isSurge && typeof ($httpAPI) !== 'undefined') {
+            return new Promise((resolve) => {
+                $httpAPI("POST", "v1/policy_groups/select", {
+                    group_name: group,
+                    policy: policy
+                }, (b) => resolve(!b.error || 0))
+            })
+        }
+        if (isLoon && typeof ($config.getPolicy) !== 'undefined') {
+            const set = $config.setSelectPolicy(group, policy);
+            return set || 0;
+        }
+        if (isQuanX && typeof ($configuration) !== 'undefined') {
+            return new Promise((resolve) => {
+                $configuration.sendMessage({
+                    action: "set_policy_state",
+                    content: {
+                        [group]: policy
+                    }
+                }).then((b) => resolve(!b.error || 0), () => resolve());
+            })
+        }
+    }
+    const get = (options, callback) => {
+        if (isQuanX) {
+            options["method"] = "GET";
+            $task.fetch(options).then(response => {
+                callback(null, adapterStatus(response), response.body)
+            }, reason => callback(reason.error, null, null))
+        }
+        if (isHTTP) {
+            if (isSurge) options.headers['X-Surge-Skip-Scripting'] = false;
+            $httpClient.get(options, (error, response, body) => {
+                callback(error, adapterStatus(response), body)
+            })
+        }
+    }
+    return {
+        getPolicy,
+        setPolicy,
+        isSurge,
+        isQuanX,
+        isLoon,
+        notify,
+        read,
+        ssid,
+        get
+    }
+}
