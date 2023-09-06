dca58cfbdbf8f6b85a9bc463853ea6a78f20a15d
SimonGino <68073491+SimonGino@users.noreply.github.com>
2023-09-06 11:36:40 +0800

    Update ipinfo-check.js

--- Surge/Panel/ipcheck/ipinfo-check.js
+++ Surge/Panel/ipcheck/ipinfo-check.js
@@ -29,7 +29,7 @@ $httpClient.get(url, function (error, response, data) {
     body = {
         title: "Data from ipinfo.io",
         content: getIP() + `\n节点IP : ${ip}\n节点ISP : ${isp}\n节点位置 : ${emoji}${country} - ${city}`,
-        icon: "key.icloud",
+        icon: "network",
         'icon-color': "#5AC8FA"
     };
     $done(body);
