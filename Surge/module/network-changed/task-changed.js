34ba10bd6a5d84abe21c7999db07d6f25ae1262e
SimonGino <68073491+SimonGino@users.noreply.github.com>
2023-11-27 11:03:58 +0800

    Update task-changed.js

--- Surge/module/network-changed/task-changed.js
+++ Surge/module/network-changed/task-changed.js
@@ -8,8 +8,8 @@ let $ = nobyda();
 
 (async () => {
     const current = await $.getPolicy(config.all_proxy);
-    const network = $network.wifi.ssid;
     if(current === 'JP-Wiki-Snell'){
+        const network = $network.wifi.ssid;
         if (network) {
             const isIncluded = config.all_switch.includes(network);
             if (isIncluded) { 
@@ -25,6 +25,7 @@ let $ = nobyda();
             $notification.post('断开公司WIFi', '', `网络切换至  ${config.all_snell}`);
         }
     }else if(current === '日本代理链'){
+        const network = $network.wifi.ssid;
         if (network) {
             if (!isIncluded) { 
                 $.setPolicy(config.all_proxy,config.all_snell)
