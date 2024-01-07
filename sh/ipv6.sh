aa6e803a5b199d9e8defe5ea8ddf66c1d31a1358
SimonGino <68073491+SimonGino@users.noreply.github.com>
2024-01-07 21:37:32 +0800

    Update ipv6.sh
    update choice

--- sh/ipv6.sh
+++ sh/ipv6.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-# Prompt the user to choose to disable or enable IPv6
-echo "你想要禁用还是启用 IPv6？（输入 'disable' 或 'enable'）"
-read choice
+# 提示用户选择是否禁用或启用 IPv6
+echo "请选择是否禁用或启用 IPv6？"
+echo "1. 禁用"
+echo "2. 启用"
+read -p "请输入选项（1 或 2）: " choice
 
-# Check the user's choice and update the IPv6 settings accordingly
-if [ "$choice" = "disable" ]; then
-    # Disable IPv6
+# 根据用户的选择更新 IPv6 设置
+if [ "$choice" = "1" ]; then
+    # 禁用 IPv6
     sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
     sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
     echo "IPv6 已被禁用。"
-elif [ "$choice" = "enable" ]; then
-    # Enable IPv6
+elif [ "$choice" = "2" ]; then
+    # 启用 IPv6
     sudo sysctl -w net.ipv6.conf.all.disable_ipv6=0
     sudo sysctl -w net.ipv6.conf.default.disable_ipv6=0
     echo "IPv6 已被启用。"
 else
-    echo "无效的选择。请键入 'disable' 或 'enable'。"
+    echo "无效的选择。请键入 '1' 或 '2'。"
 fi
 
-# Reload the configuration to apply the changes immediately
+# 重新加载配置以立即应用更改
 sudo sysctl -p
